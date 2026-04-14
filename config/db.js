43d84a6262b3e1d4e568cbeb74caff878eb6742d
vaneara <aracenavanesa@gmail.com>
2026-04-14 10:02:16 -0300

    Quito credenciales y uso .env

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://vaneara_db:0986Gael..@vaneara.y59c7ia.mongodb.net/tp_back');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Mongo conectado');
   } catch (error) {
     console.error('Error:', error);
