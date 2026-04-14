516053529c9ea4a7184dc734ff0e6aac4a4dde9a
Fernando Rodriguez <ferchulobo2015@gmail.com>
2026-04-14 14:09:15 -0300

    conexion a mongo con vscode

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ connectDB();
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en el puerto: ${PUERTO}`)
+    console.log(`Servidor corriendo en el puerto: ${PORT}`)
 })
